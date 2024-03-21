15f5e73aa77309d6942b4e5d5180fcaef047e278
kauanemuzeka <105368374+kauanemuzeka@users.noreply.github.com>
2024-03-21 08:21:01 -0300

    ok

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo")
 
-for(let j=0; i <botoes.length; i++){
+for(let i=0; i <botoes.length; i++){
     botoes[i].onclik = function(){
-        for(lrt j=0;<botoes.length;j++){
+        for(let j=0;<botoes.length;j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         
@@ -11,3 +11,7 @@ for(let j=0; i <botoes.length; i++){
 }
 botoes[i].classList.add("ativo");
 textos[i].classList.add("ativo");
+
+
+    }
+}
