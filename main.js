2343ec4babd0b614a097f31323092139cee0c4f7
kauanemuzeka <105368374+kauanemuzeka@users.noreply.github.com>
2024-03-21 08:32:10 -0300

    ok

--- main.js
+++ main.js
@@ -7,13 +7,10 @@ for(let i=0; i <botoes.length; i++){
         for(let j=0;<botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
-        
-    
-}
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-
-
+       }
+       botoes[i].classList.add("ativo");
+       textos[i].classList.add("ativo");
     }
 }
 contadores[0].textContext = "contagem Regressiva";
+const temObjetivo1 = new Date("2024-12-21");
