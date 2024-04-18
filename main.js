301e563d0db1442a2b94e7c7bd61f65655881d86
kauanemuzeka <105368374+kauanemuzeka@users.noreply.github.com>
2024-04-18 08:22:37 -0300

    ok

--- main.js
+++ main.js
@@ -15,10 +15,12 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-05T00:00:00");
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4, tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -34,3 +36,15 @@ function calculaTempo(tempoObjetivo) {
  
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
+function atualizaCronometro(){
+    //laço de repetição para interagir com todos os objetivos
+    for (let i = 0; i < contadores.length; i++){
+        contadores[i].textContent = calculaTempo(tempos[i]); //textContent mostra na tela a meta
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro(); //chamada da função criada anteriormente dentro desta função
+    setInterval(atualizaCronometro, 1000); //função que faz a contagem do tempo a cada seg.
+}
+comecaCronometro(); //chamada da função que mostra o cronômetro
